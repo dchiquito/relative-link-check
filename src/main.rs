31e579da618f54636c92298642ad21a7396d9938
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-08-08 11:29:50 -0400

    Basic arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::{Path, PathBuf};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    base: Option<PathBuf>,
+
+    #[arg()]
+    directories: Vec<PathBuf>,
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let mut args = Args::parse();
+    if args.base.is_none() {
+        args.base = Some(std::env::current_dir()?);
+    }
+    if args.directories.is_empty() {
+        args.directories.push(std::env::current_dir()?);
+    }
+    println!("Hello, world! {:?}", args.directories);
+    Ok(())
 }
